a0cdb564597f0c9b8dd96d6a7925071104036fc0
MiguelEcheverria07 <miguel.echeverria@cecar.edu.co>
2024-05-04 08:37:43 -0500

    Cambios dinamicos

--- js/utilidades.js
+++ js/utilidades.js
@@ -1,4 +1,5 @@
 var directorioImagenes = "../recursos/imagenes/"
+var ejecutarFuncion = false;
 
 function crearNodo(tipoNodo) {
     var nodo = document.createElement(tipoNodo);
@@ -12,11 +13,21 @@ function crearNodoConTexto(tipoNodo, textoNodo) {
     document.body.appendChild(nodo);
 }
 
-function crearNodoImagen(nombreImagen, alt) {
+function crearNodoImagen(nombreImagen, alt, width) {
     var nodo = crearNodo("img");
     nodo.src = directorioImagenes + nombreImagen;
     nodo.alt = alt;
+    nodo.style.width = width;
     document.body.appendChild(nodo);
 }
 
-
+function crearUI() {
+    if(!ejecutarFuncion){
+    crearNodoConTexto('h1','Ejemplo JS');
+    crearNodoConTexto("p", "Este es un ejemplo de javascript");
+    crearNodoImagen("cielo.png","Cielo PNG", "500px");
+    ejecutarFuncion = true;
+    }else{
+      alert("Relajate, ya presionaste una vez")
+    }
+}
