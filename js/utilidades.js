da4cd1db799a7aa01666df15213baee6fd21546f
MiguelEcheverria07 <miguel.echeverria@cecar.edu.co>
2024-05-10 17:38:36 -0500

    cambios

--- js/utilidades.js
+++ js/utilidades.js
@@ -9,14 +9,28 @@ function crearNodoConTexto(tipoNodo, textoNodo) {
     var nodo = document.createElement(tipoNodo);
     var textoNodo = document.createTextNode(textoNodo);
     nodo.appendChild(textoNodo);
-    document.body.appendChild(nodo);
+    return nodo;
 }
 
-function crearNodoImagen(nombreImagen, alt) {
+function crearNodoImagen(nombreImagen, alt, width) {
     var nodo = crearNodo("img");
     nodo.src = directorioImagenes + nombreImagen;
     nodo.alt = alt;
+    nodo.style.width = width;
+    return nodo;
+}
+
+function crearNodoLista(tipoNodo, textoNodo) {
+    var nodo = crearNodo(tipoNodo);
+    var textoNodo = document.createTextNode(textoNodo);
+    nodo.appendChild(textoNodo);
+    return nodo;
+}
+
+function adicionarNodoABody(nodo) {
     document.body.appendChild(nodo);
 }
 
-
+function adicionarNodoAContenedor(nodo, contenedor) {
+    contenedor.appendChild(nodo);
+}
