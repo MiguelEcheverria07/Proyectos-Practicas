c3b8a7f1d69c87020d21502e5cad3a4ce4663ab9
MiguelEcheverria07 <miguel.echeverria@cecar.edu.co>
2024-05-24 17:43:06 -0500

    cambios

---
+++ js/consumirapipaises.js
@@ -0,0 +1,30 @@
+function consumirAPIPaises(){
+    var url = 'https://restcountries.com/v3.1/independent?status=true';
+    fetch(url)
+    .then(respuesta =>{
+        return respuesta.json();
+    })
+    .then(paises =>{
+        console.log(paises)
+        for (const pais of paises) {
+            console.log("Pais: " + pais.name.official )
+            console.log("Region: " + pais.region)
+            console.log("Oficial: " + pais.name.official)
+            console.log("Capital: " + pais.capital[0])
+        }
+    })
+
+    .catch(error =>{
+        console.log(" Error", error);
+    })
+}
+
+var tabla = document.createElement("table");
+var fila = document.createElement("tr");
+var columna = document.createElement("td");
+
+
+
+
+
+
